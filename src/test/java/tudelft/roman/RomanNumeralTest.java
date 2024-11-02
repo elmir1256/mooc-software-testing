97c0ea331ba8117815d4878e963eb8fbce41f62f
Elmir Mammadov <elmirstan@gmail.com>
2024-11-02 11:42:39 +0400

    Complete test for RomanNumeralTest

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -5,32 +5,40 @@ import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
 
+//
+//    @Test
+//    public void singleNumber() {
+//        RomanNumeral roman = new RomanNumeral();
+//        int result = roman.convert("I");
+//        Assertions.assertEquals(1, result);
+//    }
+//
+//    @Test
+//    public void numberWithManyDigits() {
+//        RomanNumeral roman = new RomanNumeral();
+//        int result = roman.convert("VIII");
+//        Assertions.assertEquals(8, result);
+//    }
+//
+//    @Test
+//    public void numberWithSubtractiveNotation() {
+//        RomanNumeral roman = new RomanNumeral();
+//        int result = roman.convert("IV");
+//        Assertions.assertEquals(4, result);
+//    }
+//
+//    @Test
+//    public void numberWithAndWithoutSubtractiveNotation() {
+//        RomanNumeral roman = new RomanNumeral();
+//        int result = roman.convert("XLIV");
+//        Assertions.assertEquals(44, result);
+//    }
 
     @Test
-    public void singleNumber() {
+    public void numberWithTwoDigits(){
         RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("I");
-        Assertions.assertEquals(1, result);
-    }
+        int result = roman.convert("IX");
 
-    @Test
-    public void numberWithManyDigits() {
-        RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("VIII");
-        Assertions.assertEquals(8, result);
-    }
-
-    @Test
-    public void numberWithSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("IV");
-        Assertions.assertEquals(4, result);
-    }
-
-    @Test
-    public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("XLIV");
-        Assertions.assertEquals(44, result);
+        Assertions.assertEquals(9, result);
     }
 }
